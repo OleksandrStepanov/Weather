23f4dafd6c7ce6422cc8620eae0aae5f81858e05
oleksandrstepanov <cbrzelviv@ukr.net>
2015-12-15 17:40:44 +0000

    Add new files

---
+++ index.js
@@ -0,0 +1,9 @@
+$(function() {
+    $.getJSON('http://api.openweathermap.org/data/2.5/forecast/daily?q=Bogota&units=metric&cnt=3&appid=c795fd41bd4be40c42b0bc86ac7aa33c',
+    function(data){
+        $('#temptoday').html(data.list[0].temp.day);
+        $('#temptomorrow').html(data.list[1].temp.day);
+        $('#tempaftertomorrow').html(data.list[2].temp.day);
+    });
+    
+});
